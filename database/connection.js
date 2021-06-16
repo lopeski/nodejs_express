537b65c6ffe676071e9fbfeac0ea1b0a1b16a20a
Moises L. Apocalipse <lopeski@outlook.com>
2021-06-16 11:33:21 -0300

    change connection

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://dbdkkqpodqqlsu:a522f214366dc66742d67892f6c6098a9c4be320d26e89210975024ece811d81@ec2-54-160-96-70.compute-1.amazonaws.com:5432/dd8nkagm9a0j7d', {
+const sequelize = new Sequelize('postgres', {
   dialectOptions: {
     ssl: {
       require: true,
